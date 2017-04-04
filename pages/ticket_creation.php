b5211f91f967323f1f991a06315ebd739c816f9f
roman <roman.dilmi@orange.fr>
2017-04-04 15:46:05 +0200

    création de ticket

---
+++ pages/ticket_creation.php
@@ -0,0 +1,115 @@
+<?php
+require_once('header.html');
+?>
+
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Création de ticket</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+
+
+            <div class="row" style="margin: 0 auto;">
+                <div class="col-lg-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Veuillez renseignez les champs ci-dessous
+                        </div>
+                        <!-- /.panel-heading-->
+                        <div class="panel-body">
+                              <!--  <div class="col-lg-6">-->
+                                    <form role="form">
+                                      <fieldset disabled>
+                                          <div class="form-group">
+                                              <label for="disabledSelect">Numéro demande</label>
+                                              <input class="form-control" id="disabledInput" type="text" placeholder="0594563" disabled>
+                                          </div>
+                                      </fieldset>
+                                        <div class="form-group">
+                                            <label>Objet</label>
+                                            <input class="form-control" placeholder="Décrivez succintement votre incident">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Description</label>
+                                            <textarea class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Catégorie(s)</label>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="choix[]" value="mat">MATERIEL
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="choix[]" value="log">LOGICIEL
+                                                </label>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="choix[]" value="res">RESEAU
+                                                </label>
+                                            </div>
+                                        <div class="form-group">
+                                            <label>Priorité</label>
+                                            <select class="form-control">
+                                                <option value="blo">BLOQUANT</option>
+                                                <option value="bas">BAS</option>
+                                                <option value="mod">MODERE</option>
+                                                <option value="hau">HAUT</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Date limite de résolution</label>
+                                            <select class="form-control">
+                                                <option value="2j">2 JOURS</option>
+                                                <option value="1s">1 SEMAINE</option>
+                                                <option value="2s">2 SEMAINES</option>
+                                                <option value="1m">1 MOIS</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Localisation</label>
+                                            <input class="form-control" placeholder="Entrez IP ou nom de machine">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Pièce jointe</label>
+                                            <input type="file">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Commentaires</label>
+                                            <textarea class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn-outline btn-primary btn-lg btn-block">Envoyer</button>
+                                    </form>
+                                <!--</div>-->
+                                <!-- ./ col-lg-6 -->
+
+                          </div>
+                          <!-- ./ panel-body-->
+                    </div>
+                    <!-- /.panel panel-default-->
+                </div>
+                <!-- /.col-lg-12-->
+            </div>
+            <!-- /.row-->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
+
+</body>
+
+</html>
