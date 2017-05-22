f44eb07cf67c63c2eec8c0cde85a5d8355d3848f
Mickymicky <dodou780@hotmail.fr>
2017-05-22 09:16:57 +0200

    push form.php
    fonctions input teaxtarea, etc

--- lib/form.php
+++ lib/form.php
@@ -4,6 +4,28 @@ function input($id,$placehold){
     return "<input type='text' class='form-control' id='$id' name='$id' value='$value' placeholder='$placehold' autofocus>";
 }
 
+function inputId($id,$placehold,$value){
+    return "<input type='text' class='form-control' id='$id' name='$id' value='$value' placeholder='$placehold' disabled>";
+    
+}
+
+function textarea($id){
+    $value = isset($_POST[$id]) ? $_POST[$id] : '';
+    return "<textarea type='text' class='form-control' id='$id' name ='$id' rows='3'>$value</textarea>";
+}
+
+function select($id, $options = array()){
+    $return = "<select class='form-control' id='$id' name='$id'>";
+    foreach($options as $k => $v){
+        $selected = '';
+        if(isset($_POST[$id]) && $k == $_POST[$id]){
+            $selected = ' selected="selected"';
+        }
+        $return .= "<option value='$k' $selected>$v</option>";
+    }
+    $return .= '</select>';
+    return $return; 
+}
 
 function classError($error,$success){
     if(isset($_POST['email']) && isset($_POST['password']) && (!isset($_SESSION['Auth']))){
